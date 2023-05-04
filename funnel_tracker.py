b48f7da7793ac678af4a98ee90a20b6dff0343d3
gajdi88 <70482516+gajdi88@users.noreply.github.com>
2023-05-04 23:07:02 +0100

    Implement own comparison function

--- funnel_tracker.py
+++ funnel_tracker.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from deepdiff import DeepDiff
+import math
 import time
 import json
 import os
@@ -7,33 +7,46 @@ import os
 # We'll store the old data here.
 old_data = None
 
-def check_mods(new_data,old_data = None):
+def is_equal(a,b):
+    if a == b:
+        return True
+    if math.isnan(a) and math.isnan(b):
+        return True
+    return False
 
-    # Only act if the modified file is our target file.
-    try:
-        # Read the Excel file.
+def compare_dataframes(old_data, new_data):
+    changes = []
+    for col in new_data.columns:
+        for row in range(len(new_data)):
+            old_value = old_data.at[row, col]
+            new_value = new_data.at[row, col]
+            if not is_equal(old_value,new_value):
+                change = {
+                    'row': row,
+                    'column': col,
+                    'old_value': old_value,
+                    'new_value': new_value
+                }
+                changes.append(change)
+    return changes
 
-
-        if old_data is not None:
-            # Compare the old data with the new data.
-            diff = DeepDiff(old_data, new_data,ignore_nan_inequality=True)
-            if diff:
-                # Log the changes.
-                with open("log.txt", "a") as f:
-                    f.write(json.dumps(diff, indent=2))
-
-        # Update the old data.
-        old_data = new_data
-    except Exception as e:
-        print(f"Error: {str(e)}")
-    return old_data
+def save_changes_to_json(changes):
+    with open("log.txt", "w") as f:  # w for overwrite, a for append
+        f.write(json.dumps(changes, indent=2))
 
 if __name__ == "__main__":
 
     #while True:
-    old_data = pd.read_excel("old_funnel.xlsm",sheet_name="FY23 Growth Initiatives",header=8).to_dict()
-    new_data = pd.read_excel("funnel.xlsm", sheet_name="FY23 Growth Initiatives", header=8).to_dict()
+    print("Reading old funnel...")
+    old_data = pd.read_excel("old_funnel.xlsm",sheet_name="FY23 Growth Initiatives",header=8)
+    print("Reading new funnel...")
+    new_data = pd.read_excel("funnel.xlsm", sheet_name="FY23 Growth Initiatives", header=8)
+    print("Comparing...")
+    changes = compare_dataframes(old_data, new_data)
 
-    old_data = check_mods(new_data, old_data=old_data)
+    if changes:
+        save_changes_to_json(changes)
+    else:
+        print("no changes")
 
     #    time.sleep(1)
